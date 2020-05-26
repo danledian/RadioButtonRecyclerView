78085c5bbb7c85350a608c9a29d12813bafb6901
danledian <474608897@qq.com>
2020-05-26 13:52:16 +0800

    整理代码

--- app/src/main/java/com/hs/radiobuttonrecyclerview/MainActivity.kt
+++ app/src/main/java/com/hs/radiobuttonrecyclerview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.hs.radiobuttonrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
@@ -12,10 +14,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var singleRow: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
         val users = listOf(
             User("example"),
             User("execution"),
@@ -25,19 +33,38 @@ class MainActivity : AppCompatActivity() {
             User("particular"),
             User("decoration")
         )
-        val adapter = UserAdapter(users)
-        adapter.setOnItemClickListener(object : UserAdapter.OnItemClickListener{
-            override fun onItemClick(view: View?, position: Int) {
-                Toast.makeText(applicationContext, "$position", Toast.LENGTH_SHORT).show()
-            }
-        })
-        rv_user_list.adapter = adapter
+        rv_user_list.adapter = UserAdapter(users).apply {
+            setOnItemClickListener(object : UserAdapter.OnItemClickListener{
+                override fun onItemClick(view: View?, position: Int) {
+                    Toast.makeText(applicationContext, "$position", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
         rv_user_list.layoutManager = GridLayoutManager(this, 2)
         rv_user_list.addItemDecoration(
             DividerItemDecoration(
                 this
             )
         )
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_user_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.user_list -> {
+                updateLayoutManager()
+                true
+            }else ->
+                super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateLayoutManager() {
+        singleRow = !singleRow
+        rv_user_list.layoutManager = GridLayoutManager(this, if(singleRow) 1 else 2)
     }
 }
